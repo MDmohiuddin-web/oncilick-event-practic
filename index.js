b1f88d1a815242e597666d038d1521b97c2b9c77
MDmohiuddin <149958768+MDmohiuddin-web@users.noreply.github.com>
2024-02-15 16:38:52 +0600

    s
    s

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 
-function hideLoginForm() {
-    const form = document.getElementById('login-form');
-    form.style.display = 'none';
-    const cardsection = document.getElementById('card-section');
-    cardsection.style.display = 'contents';
+// function hideLoginForm() {
+//     const form = document.getElementById('login-form');
+//     form.style.display = 'none';
+//     const cardsection = document.getElementById('card-section');
+//     cardsection.style.display = 'contents';
    
+
     
-    
+// }
+function hideLoginForm(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const repeatPassword = document.getElementById('repeat-password').value;
+
+    if (email === '' || password === '' || repeatPassword === '') {
+        alert('Please fill in all fields');
+    } else {
+        const form = document.getElementById('login-form');
+        form.classList.add('hidden');
+        const cardsection = document.getElementById('card-section');
+        cardsection.classList.remove('hidden');
+    }
 }
 
 
 
-function  showLoginForm(){
-    if(email==="")
-    {
 
-    }
-    else{
-
-    }
-}
 
 
 
